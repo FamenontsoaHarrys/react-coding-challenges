e4e32e30d7b7f86732478cb0de76d21dead8aebb
RAVALOMANANA Famenontsoa Harrys <famenofameno01@gmail.com>
2021-07-19 01:00:40 +0300

    Update config.js

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: '5af277ae760c4c04832aa7e2559c25e2',
+    clientSecret: '60bd9d00d7ad4b119844e80f5c349f72'
   }
 }
